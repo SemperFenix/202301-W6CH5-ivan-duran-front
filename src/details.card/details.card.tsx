c2d9397fd7c32cce95a45882e0845f7eed219a8b
SemperFenix <dureivan@gmail.com>
2023-02-26 19:28:03 +0100

    Add details.card component

---
+++ src/details.card/details.card.tsx
@@ -0,0 +1,18 @@
+import { Scrub } from "../models/scrub.model";
+
+type DetailsCardProps = {
+  info: Scrub;
+};
+
+export function DetailsCard({ info }: DetailsCardProps) {
+  return (
+    <>
+      <div>
+        <img src={info.img} width="450" alt={info.name} />
+        <p>Name: {info.name}</p>
+        <p>Occupation: {info.occupattion}</p>
+        <p>Personality: {info.extend_perso}</p>
+      </div>
+    </>
+  );
+}
