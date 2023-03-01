7753c20414aa5fd7e2f6fd49a6f1555ae957fa82
SemperFenix <dureivan@gmail.com>
2023-03-01 10:37:25 +0100

    Update edit functionality

--- src/new.scrub/new.scrub.tsx
+++ src/new.scrub/new.scrub.tsx
@@ -1,4 +1,5 @@
 import { SyntheticEvent, useMemo } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useScrubs } from "../hooks/use.scrubs";
 import { Scrub } from "../models/scrub.model";
 import { ScrubsRepo } from "../services/repository/scrubs.repo";
@@ -7,10 +8,12 @@ import "./new.scrub.css";
 
 export function NewScrub() {
   const repo = useMemo(() => new ScrubsRepo(), []);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const { createScrub } = useScrubs(repo);
+  const { scrubs, createScrub, updateScrub } = useScrubs(repo);
 
-  const handleSubmit = (ev: SyntheticEvent) => {
+  const handleCreate = (ev: SyntheticEvent) => {
     ev.preventDefault();
     const formNewScrub = document.querySelector("form") as HTMLFormElement;
     const newScrub: Partial<Scrub> = {
@@ -22,11 +25,28 @@ export function NewScrub() {
     };
     createScrub(newScrub);
     formNewScrub.reset();
+    navigate("/gallery");
   };
+
+  const handleUpdate = (ev: SyntheticEvent) => {
+    ev.preventDefault();
+    const formNewScrub = document.querySelector("form") as HTMLFormElement;
+    const updatedScrub: Partial<Scrub> = {
+      name: (formNewScrub[0] as HTMLInputElement).value,
+      img: (formNewScrub[1] as HTMLInputElement).value,
+      occupattion: (formNewScrub[2] as HTMLInputElement).value,
+      personality: (formNewScrub[3] as HTMLInputElement).value,
+      extendPerso: (formNewScrub[4] as HTMLInputElement).value,
+    };
+    updateScrub({ ...scrubs.actualScrub, ...updatedScrub });
+    formNewScrub.reset();
+    navigate("/gallery");
+  };
+  const handler = id === "add" ? handleCreate : handleUpdate;
 
   return (
     <>
-      <form className="add-scrub" data-testid="form" onSubmit={handleSubmit}>
+      <form className="add-scrub" data-testid="form" onSubmit={handler}>
         <input
           className="add-scrub_input"
           type="text"
